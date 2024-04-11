a6e3eba01f38b29d9d8edc1a38cf60666c1676f5
Jonathan <jonathan.condori.2013@gmail.com>
2024-04-10 19:34:55 -0500

    add saludos5

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ function saludos(){
     return true;
 }
 
-function saludos(){
+function saludos2(){
+    return true;
+}
+
+function saludos3(){
+    return true;
+}
+
+function saludos4(){
+    return true;
+}
+
+function saludos5(){
     return true;
 }
